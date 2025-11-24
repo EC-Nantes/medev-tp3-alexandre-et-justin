43bc68ac7661612c1fa4126dcad3de3586bcbe37
JusiBridou <juslamarcade@gmail.com>
2025-11-24 16:53:27 +0100

    construction de la classe Écriture

--- PictureComparator/src/main/java/org/centrale/infosi/picturecomparator/PictureWriting.java
+++ PictureComparator/src/main/java/org/centrale/infosi/picturecomparator/PictureWriting.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.infosi.picturecomparator;
+import java.io.*;
 
 /**
  * La classe permettant d'écrire 
@@ -10,4 +11,32 @@ package org.centrale.infosi.picturecomparator;
  */
 public class PictureWriting {
     
+    // Écriture d'une image en PGM
+    public static void ecrirePGM(Picture pic, String filename) throws IOException {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(filename))) {
+            pw.println("P2");
+            pw.println("# Fichier de l'image : "+ filename +" crée par le programme de Justin et Alexandre !");
+            pw.println(pic.getWidth() + " " + pic.getHeight());
+            pw.println(pic.getMaxGray());
+
+            int count = 0;
+            int[][] pixels = pic.getPixels();
+
+            for (int i = 0; i < pic.getHeight(); i++) {
+                for (int j = 0; j < pic.getWidth(); j++) {
+                    String val = pixels[i][j] + " ";
+                    pw.print(val);
+                    count += val.length();
+
+                    // Respect de la contrainte : max 70 caractères par ligne
+                    if (count >= 70) {
+                        pw.println();
+                        count = 0;
+                    }
+                }
+                pw.println();
+                count = 0;
+            }
+        }
+    }
 }
