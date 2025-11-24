0fb717cd3488c27b84dbc08315fe97eb3fcea79d
JusiBridou <juslamarcade@gmail.com>
2025-11-24 16:38:16 +0100

    construction de la classe Écriture

---
+++ PictureComparator/src/main/java/org/centrale/infosi/picturecomparator/PictureWriting.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.infosi.picturecomparator;
+
+/**
+ * La classe permettant d'écrire 
+ * @author jujus
+ */
+public class PictureWriting {
+    
+}
