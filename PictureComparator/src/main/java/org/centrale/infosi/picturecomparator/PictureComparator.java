df6f8663922715ba45e7d9b38c29206402d12129
JusiBridou <juslamarcade@gmail.com>
2025-11-24 17:18:06 +0100

    Test Écriture (ça marche)

--- PictureComparator/src/main/java/org/centrale/infosi/picturecomparator/PictureComparator.java
+++ PictureComparator/src/main/java/org/centrale/infosi/picturecomparator/PictureComparator.java
@@ -5,12 +5,31 @@
 package org.centrale.infosi.picturecomparator;
 
 /**
- *
+ * test de la méthode d'écriture
  * @author Catherine
  */
 public class PictureComparator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Création d'une image 10x10 avec un dégradé simple
+        Picture pic = new Picture(10, 10);
+
+        for (int i = 0; i < pic.getHeight(); i++) {
+            for (int j = 0; j < pic.getWidth(); j++) {
+                // Dégradé horizontal : valeur proportionnelle à la colonne
+                int value = (j * 255) / pic.getWidth();
+                pic.setPixelPrecis(i, j, value);
+            }
+        }
+
+        // Utilisation de PictureWriting pour écrire l'image
+        PictureWriting Ecrivain = new PictureWriting(pic);
+        try {
+            Ecrivain.ecrirePGM("test.pgm");
+            System.out.println("Image écrite dans test.pgm !");
+        } catch (Exception e) {
+            System.err.println("Erreur lors de l'écriture : " + e.getMessage());
+        }
+
     }
 }
