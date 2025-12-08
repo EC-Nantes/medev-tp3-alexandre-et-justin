444f43b7235d66ef4c06aa4f3278b3ea077f647e
Alexandre Mestre <alexandre>
2025-12-08 14:47:21 +0100

    ajout de la différence entre 2 images

--- PictureComparator/src/main/java/org/centrale/infosi/picturecomparator/PictureTransform.java
+++ PictureComparator/src/main/java/org/centrale/infosi/picturecomparator/PictureTransform.java
@@ -12,7 +12,7 @@ public class PictureTransform {
 	public PictureTransform() {}
 	
 	
-	Picture greyToBlackAndWhite(Picture pic, int seuil) {
+	public Picture greyToBlackAndWhite(Picture pic, int seuil) {
 		Picture p = new Picture(pic);
 		
 		int width = pic.getWidth();
@@ -25,6 +25,30 @@ public class PictureTransform {
 					p.setPixelPrecis(i, j, max_value);
 				} else {
 					p.setPixelPrecis(i, j, 0);
+				}
+			}
+		}
+		return p;
+	}
+	
+	public Picture pictureDiff(Picture picA, Picture picB) {
+		if (picA.getWidth() != picB.getWidth() || picA.getHeight() != picB.getHeight()) {
+			return null;
+		}
+		
+		Picture p = new Picture(picA);
+		int width = p.getWidth();
+		int height = p.getHeight();
+		
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int pixel = picA.getPixelPrecis(i, j) - picB.getPixelPrecis(i, j);
+				
+				// Si le niveau de gris du pixel est négatif, on met l'opposé
+				if (pixel < 0) {
+					p.setPixelPrecis(i, j, -pixel);
+				} else {
+					p.setPixelPrecis(i, j, pixel);
 				}
 			}
 		}
